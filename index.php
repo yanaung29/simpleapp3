df75f42d694e432a9b0b6fecd1bc73d6c56abd01
yanaung <yanaung29@gmail.com>
2023-09-21 15:10:30 +0800

    changes

--- index.php
+++ index.php
@@ -6,11 +6,8 @@
 
 <body> 
 	<h1>PHP Test</h1>
-		<p><b>An Example of PHP in Action</b></p>
+		<p><b>An Example of PHP in Action LAB9</b></p>
 		<?php echo "The Current Date and Time is: <br />"; 
 		echo date("g:i A l, F j Y.");?> </p>
-	<h2>PHP Information</h2> 
-		<p> <?php phpinfo(); ?> </p> 
-        <h2>TIME FOR A COLD BEER</h2>
 	</body> 
 </html>
